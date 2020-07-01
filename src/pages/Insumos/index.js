a4d988f08be1fcce44bc20820d95a0a4b0b3d50f
Jhordan <eriquy@gmail.com>
2020-07-01 11:45:53 -0300

    add pg insumos

---
+++ src/pages/Insumos/index.js
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+import './style.css';
+
+
+export default function Insumos(){
+    return (
+        <h1 className="display-1">Insumos</h1>
+    )
+}
--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch} from 'react-router-dom';
 import Logon from './pages/Logon';
+import Insumos from './pages/Insumos';
 
 export default function Routes(){
     return (
         <BrowserRouter>
             <Switch>
                 <Route path="/" exact component={Logon} />
+                <Route path="/insumos" exact component={Insumos} />
             </Switch>
         </BrowserRouter>
     )
